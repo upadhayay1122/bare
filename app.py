2f826843af4f0c62340ce3e32ffb0ce445b033c2
gaurav <upadhayay.1122@outlook.com>
2020-08-24 00:35:36 +0530

    Commit for the working Flash Form with Flash messages

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-from flask import Flask, render_template,request
+from flask import Flask,request,render_template,session,url_for,redirect,flash
+from flask_wtf import FlaskForm
+from wtforms import (StringField,BooleanField,RadioField,DateTimeField,
+                     SelectField,TextField,TextAreaField,SubmitField,PasswordField)
+
+from wtforms.validators import IPAddress,DataRequired
+
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mykey'
+
+class Adddevice(FlaskForm):
+
+    ipaddress = StringField('Enter the Device Ipaddress',validators=[IPAddress()],render_kw={'placeholder': 'Ipaddress'})
+    username = StringField('Enter the Device Username', validators=[DataRequired()],render_kw={'placeholder': 'Device Username'})
+    password = PasswordField('Enter the Device Password',validators=[DataRequired()],render_kw={'placeholder': 'Password'})
+    Groupname = SelectField(u'Select the Group name for the Device',choices=[('Cisco_router','Cisco_IOS'),
+                                                  ('Cisco_Switch','Cisco_nxos'),('Fortigate','Fortios')])
+
+    submit = SubmitField('Submit')
+
 
 @app.route('/')
 def index():
@@ -86,9 +105,43 @@ def confirmation():
     return render_template('confirmation.html',confirm = confirm, num_check = oct_numeric, ip_len_check = ip_address_lenth, ip_range_check = oct_num_range)
 
 
-@app.route('/Add-Device')
+@app.route('/Add-Device',methods=['GET','POST'])
 def adddevice():
-    return render_template('add-device.html')
+    form = Adddevice()
+
+    if request.method == 'POST':
+        oct_numberic = False
+        ip_address_lenth = False
+        oct_num_range = False
+        ipaddress = request.form['ipaddress']
+        ipaddress = ipaddress.split('.')
+        oct_numeric = all(c.isnumeric() for c in ipaddress)
+        if oct_numeric:
+            oct_num_range = all(0 < int(c) <= 255 for c in ipaddress)
+
+            if len(ipaddress) == 4:
+                ip_address_lenth = True
+
+
+        if (not oct_numeric or not ip_address_lenth or not oct_num_range) :
+            flash("Provide the correct ip address as per the IPV4 Rules!")
+            return redirect(request.url)
+
+    if form.validate_on_submit():
+
+        session['ipaddress'] = form.ipaddress.data
+        session['username'] = form.username.data
+        session['password'] = form.password.data
+        session['Groupname'] = form.Groupname.data
+
+        return redirect(url_for('thankyou'))
+    return render_template('add-device-1.html',form=form)
+
+    # return render_template('add-device.html')
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=80)
